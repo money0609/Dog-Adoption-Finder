15c9b21a5e58c419981f41ba2cf288bc9a11d32c
money0609 <38477637+money0609@users.noreply.github.com>
2025-02-03 14:45:18 -0600

    Reverted the gh-pages, updated 404 and logo href

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Search from './pages/Search';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import NotFound from './pages/404'
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Route path="/" element={<Search />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/profile" element={<Profile />} />
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </Router>
         </>
